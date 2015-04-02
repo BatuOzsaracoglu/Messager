2867066c84a4ccc01e656579008372c9d7323691
Batu Ozsaracoglu <batuoz@yahoo.com>
2015-04-02 10:21:22 -0500

    Added code to BackEnd

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BackEnd {
 
+	public List<String> getMessage(String username){
+		List<String> messages = new ArrayList<String>();
+		messages.add("Hello");
+		return messages;
+	}
+	
+	public void addMessage(String username, String msg){
+		
+	}
+	
+	public List<String> getUsers(){
+		List<String> users = new ArrayList<String>();
+		users.add("Bob");
+		return users;
+	}
+	
 }

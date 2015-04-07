8ba03dc4f650a000285fa14c52b88c96522b5b03
Fenikusu <tanyasltatiana@gmail.com>
2015-04-07 10:17:35 -0500

    Almost done Frontend

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -18,6 +18,8 @@ public class FrontEnd extends JApplet implements ActionListener {
 	public JButton G=new JButton("Get Messages");
 	public JButton PICK=new JButton("OFF");
 	public JPanel p = new JPanel();
+	public String A[] = new String[10];
+	public JComboBox List = new JComboBox(A);
 	public static final Font font = new Font("Castellan", Font.BOLD, 25);
 	public static final Font font1 = new Font("Calibri", Font.CENTER_BASELINE,
 			20);
@@ -29,27 +31,12 @@ public class FrontEnd extends JApplet implements ActionListener {
 		
 		JTextField[] g = { Name };
 		JTextArea[] g1 = { Msg };
-		this.setPreferredSize(new Dimension(1000, 700));
+		this.setPreferredSize(new Dimension(600, 300));
 		
 		Name.setPreferredSize(new Dimension(50, 20));
-		Msg.setPreferredSize(new Dimension(100, 200));
+		Msg.setPreferredSize(new Dimension(2000, 3000));
+	
 		
-		/////top
-		
-		JPanel a = new JPanel();
-		a.add(Box.createVerticalStrut(10));
-		a.setLayout(new BoxLayout(a, BoxLayout.X_AXIS));
-		JLabel welc = new JLabel("Welcome");
-		welc.setFont(font);
-		// welc.setForeground(Color.BLUE);
-		a.add(welc, BorderLayout.CENTER);
-		a.add(Box.createVerticalStrut(10));
-		JButton info=new JButton("Information");
-		info.addActionListener(this);
-		info.setActionCommand("info");
-		a.add(info, BorderLayout.LINE_END);
-		a.add(Box.createVerticalStrut(10));
-
 
 
 		// //////////////
@@ -72,61 +59,58 @@ public class FrontEnd extends JApplet implements ActionListener {
 		
 		JPanel pSubaa = new JPanel();
 		pSubaa.setLayout(new BoxLayout(pSubaa, BoxLayout.Y_AXIS));
-		JLabel s1 = new JLabel("Select Username ");
-		s.setFont(font1);
+		JLabel s1 = new JLabel("Select Username                       ");
+		s1.setFont(font1);
 		pSubaa.add(s1);
 		
-		////////////////////////////////////////////////////////////////////////////*ToDo: Dropdown menu*/
+		List.setSelectedIndex(5);
+		List.addActionListener(this);
+		List.setActionCommand("List");
+		pSubaa.add(List);
 		pSuba.add(pSubaa);
 		
 		pSub.add(pSuba);
-		pSub.add(Box.createVerticalStrut(10));
+	
 		
-		
-		
-		
-		/*R.addActionListener(this);
-		R.setActionCommand("Red");
-		pSuba.add(R);*/
-		pSub.add(pSuba);
-		pSub.add(Box.createVerticalStrut(10));
 		// //////////////// 
 
 		JPanel pSubb = new JPanel();
-		pSubb.setLayout(new BoxLayout(pSubb, BoxLayout.Y_AXIS));
+		pSubb.setLayout(new BoxLayout(pSubb, BoxLayout.X_AXIS));
 		JLabel sa = new JLabel("Message ");
 		sa.setFont(font1);
-		pSubb.add(sa);
-		pSuba.add(g1[0]);
-		/*G.addActionListener(this);
-		G.setActionCommand("Green");
-		pSubb.add(G);*/
+		
+		pSubb.add(sa,BorderLayout.PAGE_START);
+		
+		JPanel pSubba = new JPanel();
+		pSubba.setLayout(new BoxLayout(pSubba, BoxLayout.X_AXIS));
+		pSubba.add(g1[0]);
+		
 		pSub.add(pSubb);
-		pSub.add(Box.createVerticalStrut(10));
+		pSub.add(pSubba);
 //////////
 		JPanel pSubc = new JPanel();
 		pSubc.setLayout(new BoxLayout(pSubc, BoxLayout.X_AXIS));
-		JLabel n1 = new JLabel("Click this to make the window Blue ");
-		n1.setFont(font1);
-		pSubc.add(n1);
-
-		B.addActionListener(this);
-		B.setActionCommand("Blue");
-		pSubc.add(B);
+		
+		G.addActionListener(this);
+		G.setActionCommand("Get");
+		pSubc.add(G);
+		
+		S.addActionListener(this);
+		S.setActionCommand("Send");
+		pSubc.add(S);
 		pSub.add(pSubc);
-		pSub.add(Box.createVerticalStrut(10));
+		
 		//////////////////
 		JPanel pSubd = new JPanel();
 		pSubd.setLayout(new BoxLayout(pSubd, BoxLayout.X_AXIS));
 		JLabel n2 = new JLabel("Color Picker  ");
-		n2.setFont(font1);
 		pSubd.add(n2);
 
 		PICK.addActionListener(this);
 		PICK.setActionCommand("PICK");
 		pSubd.add(PICK);
 		pSub.add(pSubd);
-		pSub.add(Box.createVerticalStrut(20));
+	
 		///////////
 		JPanel pSube = new JPanel();
 		pSube.setLayout(new BoxLayout(pSube, BoxLayout.X_AXIS));
@@ -148,52 +132,20 @@ public class FrontEnd extends JApplet implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		String cmd = arg0.getActionCommand();
-		if (cmd.equals("Red")) {
+		if (cmd.equals("Get")) {
 
-			if (R.getText().equals("ON")) {
-				R.setText("OFF");
-				p.setBackground(new Color(200,200,200));
-			}
-			else
-			{
-				R.setText("ON");
-				G.setText("OFF");
-				B.setText("OFF");
-				PICK.setText("OFF");
-				p.setBackground(new Color(200,0,0));
-			}
+			/////////////////////////
 		}
 		
-		if (cmd.equals("Green")) {
+		if (cmd.equals("Send")) {
 
-			if (G.getText().equals("ON")) {
-				G.setText("OFF");
-				p.setBackground(new Color(200,200,200));
-			}
-			else
-			{
-				G.setText("ON");
-				R.setText("OFF");
-				B.setText("OFF");
-				PICK.setText("OFF");
-				p.setBackground(new Color(0,200,0));
-			}
-		}
 		
-		if (cmd.equals("Blue")) {
+			////////////////////////
+		}
+		if (cmd.equals("List")) {
 
-			if (B.getText().equals("ON")) {
-				B.setText("OFF");
-				p.setBackground( new Color(200,200,200));
-			}
-			else
-			{
-				B.setText("ON");
-				G.setText("OFF");
-				R.setText("OFF");
-				PICK.setText("OFF");
-				p.setBackground( new Color(0,0,200));
-			}
+			
+			////////////////////////
 		}
 		
 		if (cmd.equals("PICK")) {
@@ -202,12 +154,11 @@ public class FrontEnd extends JApplet implements ActionListener {
 				PICK.setText("OFF");
 				p.setBackground( new Color(200,200,200));
 			}
+			
+	
 			else
 			{
 				PICK.setText("ON");
-				G.setText("OFF");
-				B.setText("OFF");
-				R.setText("OFF");
 				Color c = JColorChooser.showDialog(this, "Manual Pick", p.getBackground());
 				if (c != null)
 				{
@@ -221,7 +172,7 @@ public class FrontEnd extends JApplet implements ActionListener {
 
 		
 	public static void main(String[] args) {
-		JFrame f = new JFrame("Tatiana Larina, Batu Ozsaracoglu");
+		JFrame f = new JFrame("MESSAGER");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		f.setSize(dim.width/2,dim.height);
